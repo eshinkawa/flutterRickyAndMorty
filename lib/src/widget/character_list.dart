89c111de3cdfddf4d96a1caf0a35eb71d271c6f0
eshinkawa <eshinkawa@gmail.com>
2018-07-29 04:32:51 -0300

    circular image

--- lib/src/widget/character_list.dart
+++ lib/src/widget/character_list.dart
@@ -4,33 +4,46 @@ import '../model/characterModel.dart';
 class CharacterList extends StatelessWidget {
   final List<CharacterModel> characters;
   CharacterList(this.characters);
-  
+
   Widget build(context) {
     return ListView.builder(
-      itemCount: characters.length,
-      itemBuilder: (context, int index) {
-        return Container(
-      decoration: BoxDecoration(boxShadow: [
-        BoxShadow(
-          color: Colors.blueGrey,
-          blurRadius: 100.0,
-        ),
-      ]),
-      child: 
-        new ListTile(
-          contentPadding: EdgeInsets.only(top: 25.0, bottom: 15.0, left: 10.0, right: 10.0),
-          leading: Image.network(characters[index].image, height: 90.0,),
-          title: Text(characters[index].name, style: TextStyle(
-            fontSize: 22.0,
-            color: Colors.black87,
-          )),
-          subtitle: Text(characters[index].species + ' - ' + characters[index].status, style: TextStyle(
-            fontSize: 15.0,
-            color: Colors.black54,
-          ))
-        ),
-    );
-      }
-    );
+        itemCount: characters.length,
+        itemBuilder: (context, int index) {
+          return Container(
+            decoration: BoxDecoration(boxShadow: [
+              BoxShadow(
+                color: Colors.blueGrey,
+                blurRadius: 100.0,
+              ),
+            ]),
+            child: new ListTile(
+                contentPadding: EdgeInsets.only(
+                    top: 25.0, bottom: 15.0, left: 10.0, right: 10.0),
+                leading: Container(
+                  width: 90.0,
+                  height: 90.0,
+                  decoration: new BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: new DecorationImage(
+                      fit: BoxFit.fill,
+                      image: NetworkImage(characters[index].image),
+                    ),
+                  ),
+                ),
+                title: Text(characters[index].name,
+                    style: TextStyle(
+                      fontSize: 22.0,
+                      color: Colors.black87,
+                    )),
+                subtitle: Text(
+                    characters[index].species +
+                        ' - ' +
+                        characters[index].status,
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      color: Colors.black54,
+                    ))),
+          );
+        });
   }
 }
